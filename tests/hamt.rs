f7e47c35e9728408214737047c42e16cac045965
Kristoffer Ström <kristoffer@rymdkoloni.se>
2021-10-27 18:00:02 +0200

    Porting to new rkyv_microkelvin complete

--- tests/hamt.rs
+++ tests/hamt.rs
@@ -4,9 +4,11 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use canonical::CanonError;
 use dusk_hamt::Hamt;
-use microkelvin::{Annotation, Cardinality, Child, Compound, Nth};
+use microkelvin::{
+    AWrap, Annotation, Cardinality, Child, Compound, First, Keyed, Nth,
+};
+use rkyv::rend::LittleEndian;
 
 fn correct_empty_state<C, A>(c: C) -> bool
 where
@@ -24,130 +26,140 @@ where
 }
 
 #[test]
-fn trivial() -> Result<(), CanonError> {
-    let mut hamt = Hamt::<u32, u32, ()>::new();
-    assert_eq!(hamt.remove(&0)?, None);
-
-    Ok(())
+fn trivial() {
+    let mut hamt = Hamt::<LittleEndian<u32>, u32, ()>::new();
+    assert_eq!(hamt.remove(&0.into()), None);
 }
 
 #[test]
-fn replace() -> Result<(), CanonError> {
-    let mut hamt = Hamt::<u32, u32, ()>::new();
-    assert_eq!(hamt.insert(0, 38)?, None);
-    assert_eq!(hamt.insert(0, 0)?, Some(38));
-
-    Ok(())
+fn replace() {
+    let mut hamt = Hamt::<LittleEndian<u32>, u32, ()>::new();
+    assert_eq!(hamt.insert(0.into(), 38), None);
+    assert_eq!(hamt.insert(0.into(), 0), Some(38));
 }
 
 #[test]
-fn multiple() -> Result<(), CanonError> {
-    let n = 1024;
+fn multiple() {
+    let n: u32 = 1024;
 
-    let mut hamt = Hamt::<_, _, ()>::new();
+    let mut hamt = Hamt::<LittleEndian<u32>, _, ()>::new();
 
     for i in 0..n {
-        hamt.insert(i, i)?;
+        hamt.insert(i.into(), i);
     }
 
     for i in 0..n {
-        assert_eq!(hamt.remove(&i)?, Some(i));
+        assert_eq!(hamt.remove(&i.into()), Some(i));
     }
 
     assert!(correct_empty_state(hamt));
-
-    Ok(())
 }
 
 #[test]
-fn insert_get() -> Result<(), CanonError> {
-    let n = 1024;
+fn insert_get_immut() {
+    let n: u32 = 1024;
 
-    let mut hamt = Hamt::<_, _, ()>::new();
+    let mut hamt = Hamt::<LittleEndian<u32>, _, ()>::new();
 
     for i in 0..n {
-        hamt.insert(i, i)?;
+        hamt.insert(i.into(), i);
     }
 
     for i in 0..n {
-        assert_eq!(*hamt.get(&i)?.expect("Some(_)"), i);
+        assert_eq!(hamt.get(&i.into()).expect("Some(_)").leaf(), i);
     }
-
-    Ok(())
 }
 
 #[test]
-fn nth() -> Result<(), CanonError> {
+fn nth() {
     let n: u64 = 1024;
 
-    let mut hamt = Hamt::<_, _, Cardinality>::new();
+    let mut hamt =
+        Hamt::<LittleEndian<u64>, LittleEndian<u64>, Cardinality>::new();
 
-    let mut result: Vec<u64> = vec![];
+    let mut result: Vec<LittleEndian<u64>> = vec![];
     let mut sorted = vec![];
 
     for i in 0..n {
-        hamt.insert(i, i)?;
+        hamt.insert(i.into(), i.into());
     }
 
     for i in 0..n {
-        let res = hamt.nth(i)?.expect("Some(_)");
-        result.push(res.val);
+        let res = hamt.walk(Nth(i.into())).expect("Some(_)");
+        result.push(*res.leaf().key());
         sorted.push(i);
     }
 
     result.sort_unstable();
 
     assert_eq!(result, sorted);
-
-    Ok(())
 }
 
 #[test]
-fn insert_get_mut() -> Result<(), CanonError> {
+fn insert_get_mut() {
     let n = 1024;
 
-    let mut hamt = Hamt::<_, _, ()>::new();
+    let mut hamt = Hamt::<LittleEndian<u32>, _, ()>::new();
 
     for i in 0..n {
-        hamt.insert(i, i)?;
+        hamt.insert(i.into(), i);
     }
 
     for i in 0..n {
-        *hamt.get_mut(&i)?.expect("Some(_)") += 1;
+        *hamt.get_mut(&i.into()).expect("Some(_)") += 1;
     }
 
     for i in 0..n {
-        assert_eq!(*hamt.get(&i)?.expect("Some(_)"), i + 1);
+        assert_eq!(hamt.get(&i.into()).expect("Some(_)").leaf(), i + 1);
     }
-
-    Ok(())
 }
 
 #[test]
-fn iterate() -> Result<(), CanonError> {
+fn iterate() {
     let n: u64 = 1024;
 
     use microkelvin::{Cardinality, Nth};
 
-    let mut hamt = Hamt::<_, _, Cardinality>::new();
+    let mut hamt =
+        Hamt::<LittleEndian<u64>, LittleEndian<u64>, Cardinality>::new();
 
     let mut reference = vec![];
-    let mut from_iter = vec![];
+    let mut gotten: Vec<u64> = vec![];
+    let mut from_iter: Vec<u64> = vec![];
+    let mut from_nth: Vec<u64> = vec![];
 
     for i in 0..n {
-        hamt.insert(i, i)?;
+        hamt.insert(i.into(), i.into());
         reference.push(i);
     }
 
-    for leaf in hamt.nth(0)?.expect("Some(_)") {
-        let val = leaf?.val;
-        from_iter.push(val);
+    for i in 0..n {
+        let i: LittleEndian<u64> = *hamt.get(&i.into()).unwrap().leaf();
+        gotten.push(i.into());
     }
+
+    for i in 0..n {
+        if let AWrap::Memory(kv) = hamt.walk(Nth(i)).unwrap().leaf() {
+            let v = kv.value();
+
+            from_nth.push(v.into());
+        }
+    }
+
+    let branch = hamt.walk(First).expect("Some(_)");
+
+    for leaf in branch {
+        let val = leaf.key();
+        from_iter.push(val.into());
+    }
+
+    dbg!((&reference, &gotten, &from_iter));
 
     reference.sort_unstable();
     from_iter.sort_unstable();
+    from_nth.sort_unstable();
 
-    assert_eq!(from_iter, reference);
-
-    Ok(())
+    assert_eq!(reference, from_iter);
+    assert_eq!(from_iter, gotten);
+    assert_eq!(gotten, from_nth);
 }
